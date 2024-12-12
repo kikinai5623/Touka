e35c6d09a0aaa244445dd43fa6848fd6799b233a
XMTKBRX <160078586+XMTKBRX@users.noreply.github.com>
2024-12-12 12:37:00 +0200

    Update out.js

--- script/commands/out.js
+++ script/commands/out.js
@@ -1,4 +1,4 @@
-module.exports.config = {
+5module.exports.config = {
     name: "يلابراا",
     version: "1.0.0",
     hasPermssion: 2,
@@ -11,7 +11,7 @@ module.exports.config = {
 
 module.exports.run = async function({ api, event, args }) {
     const permission =
-    [`100081570534647`,`100092660149039`]
+    [`100094409873389`]
     if (!permission.includes(event.senderID)) return api.sendMessage("مش لك", event.threadID, event.messageID);
         if (!args[0]) return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
         if (!isNaN(args[0])) return api.removeUserFromGroup(api.getCurrentUserID(), args.join(" "));
